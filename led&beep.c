8e05c74e66c5bc974db910228dc15e6e29848f8f
Owen1u <49264931+Owen1u@users.noreply.github.com>
2020-04-22 21:59:53 +0800

    Update and rename led.c to led&beep.c

--- led&beep.c
+++ led&beep.c
@@ -3,7 +3,7 @@
 
 void LED_Init(){
   GPIO_InitTypeDef GPIO_Initstructure;
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);     //RCC->APB2ENR|=1<<3;
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);     //RCC->APB2ENR|=1<<3; Pin_Beep:PB8
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE,ENABLE);
   GPIO_Initstructure.GPIO_Mode=GPIO_Mode_Out_PP;
   GPIO_Initstructure.GPIO.Pin=GPIO_Pin_5;
